4adb101244bf9bc29209bd9366309435787e48a0
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-09-01 18:10:13 -0400

    Time: 663 ms (97.24%), Space: 17.8 MB (86.17%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,21 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
-        for i, value in enumerate(nums):
-            if i > 0 and value == nums[i - 1]:
-                continue
-            
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                tsum = value + nums[l] + nums[r]
-                if tsum < 0:
-                    l += 1
-                elif tsum > 0:
-                    r -= 1
+        def search_pair(nums, target, left, res):
+            right = len(nums) - 1
+            while left < right:
+                sum = nums[left] + nums[right]
+                if sum < target:
+                    left += 1
+                elif sum > target:
+                    right -= 1
                 else:
-                    res.append([value, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
+                    res.append([-target, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            search_pair(nums, -nums[i], i + 1, res)
         return res
