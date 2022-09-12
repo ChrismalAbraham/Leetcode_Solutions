73a5ac2abc3e9572bc54db67bfb5656f3c76cf74
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-09-12 16:14:48 -0400

    Time: 62 ms (27.02%), Space: 13.8 MB (66.69%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,10 +1,11 @@
 from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counter = Counter(s)
+        counter = Counter(s) # counters for the english alphabet are constant space
         num_pairs = 0
         for count in counter.values():
             if count > 1:
                 num_pairs += count // 2
-        if num_pairs * 2 < len(s): return num_pairs * 2 + 1
-        return num_pairs * 2
+        max_length = num_pairs * 2
+        if num_pairs * 2 < len(s): max_length += 1
+        return max_length
