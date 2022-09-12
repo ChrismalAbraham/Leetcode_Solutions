2fb8d962d03499b5e052daf1f355bdd1f890b1e5
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-09-12 15:36:19 -0400

    Time: 57 ms (88.66%), Space: 17.3 MB (94.39%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -6,15 +6,17 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def detectCycleStart(head, tor):
+            hare2 = head
+            while hare2:
+                if hare2 == tor: return tor
+                hare2 = hare2.next
+                tor = tor.next
         if not head: return
         tor, hare = head, head
         while hare and hare.next:
             tor = tor.next
             hare = hare.next.next
             if tor == hare:
-                hare2 = head
-                while hare2:
-                    if hare2 == tor: return tor
-                    hare2 = hare2.next
-                    tor = tor.next
+                return detectCycleStart(head, tor)
         return
