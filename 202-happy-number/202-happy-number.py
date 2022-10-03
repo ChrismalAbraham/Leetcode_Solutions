301f04206d14f6eaa8d377a2575fd2247047f054
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-03 18:26:54 -0400

    Time: 67 ms (33.38%), Space: 13.8 MB (96.79%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,20 +1,17 @@
 class Solution:
     def isHappy(self, num: int) -> bool:
         def next_happy_number(n: int) -> int:
-            n = str(n)
             next_n = 0
-            for char in n:
-                next_n += int(char) ** 2
+            while n > 0:
+                digit = n % 10
+                next_n += digit * digit
+                n //= 10
             return next_n
 
         slow = fast = num
-        is_happy = True
-        while fast != 1:
+        while True:
             slow = next_happy_number(slow)
             fast = next_happy_number(next_happy_number(fast))
-
-            if fast == slow and fast != 1:
-                is_happy = False
+            if slow == fast:
                 break
-
-        return is_happy
+        return slow == 1
