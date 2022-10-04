ace8468152c50398432a6323f255138a60757b87
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-04 19:08:56 -0400

    Time: 307 ms (39.29%), Space: 14.9 MB (50.41%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -8,14 +8,13 @@ class Solution:
         while i < len(a_arr) and j < len(b_arr):
 
             is_intersect = a_arr[i][start] <= b_arr[j][end] and a_arr[i][end] >= b_arr[j][start]
-            #b_intersect_a = b_arr[j][start] >= a_arr[i][start] and b_arr[j][start] <= a_arr[i][end]
 
             if is_intersect:
                 intersect_start = max(a_arr[i][start], b_arr[j][start])
                 intersect_end = min(a_arr[i][end], b_arr[j][end])
                 intersect_intervals.append([intersect_start, intersect_end])
 
-            if a_arr[i][end] < b_arr[j][end]:
+            if a_arr[i][end] <= b_arr[j][end]:
                 i += 1
             else:
                 j += 1
