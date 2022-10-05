24515f04c10fd31be9d3184bef73cf6ae01fdb7e
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-05 19:01:23 -0400

    Time: 2082 ms (5.13%), Space: 28.3 MB (30.99%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -9,3 +9,5 @@ class Solution:
                 if i != j:
                     return arr[i]
                 i += 1
+        # this is not the solution since it modifies the arry
+        # the actual solution is using tortoise and hare I think
