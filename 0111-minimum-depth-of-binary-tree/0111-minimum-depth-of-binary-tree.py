98a40b77bad9d3f5b87f57c95cf5944471111689
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-10 19:21:10 -0400

    Time: 581 ms (92.83%), Space: 54.7 MB (31.60%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -6,22 +6,29 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
+        if root is None: return 0
+        if root.left is None: return 1 + self.minDepth(root.right)
+        if root.right is None: return 1 + self.minDepth(root.left)
+        minimum_depth = min(self.minDepth(root.left), self.minDepth(root.right))
+        return 1 + minimum_depth
+         
+# Breadth first search solution using deque
+#         if root is None:
+#             return 0
 
-        queue = deque()
-        queue.append(root)
-        min_depth = 1
-        while queue:
-            level_len = (len(queue))
-            for _ in range(level_len):
-                node = queue.popleft()
-                if node.right is None and node.left is None:
-                    return min_depth
+#         queue = deque()
+#         queue.append(root)
+#         min_depth = 1
+#         while queue:
+#             level_len = (len(queue))
+#             for _ in range(level_len):
+#                 node = queue.popleft()
+#                 if node.right is None and node.left is None:
+#                     return min_depth
 
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            min_depth += 1
-        return min_depth
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             min_depth += 1
+#         return min_depth
