5d2247339185fea2fd464cbddc83ad80c7ebf775
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-17 12:12:49 -0400

    Time: 173 ms (18.88%), Space: 14 MB (74.85%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,27 +1,13 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows < 2:
+        if numRows == 1:
             return s
-        top, start, bottom = 0, 0, numRows - 1
-        # (numRows - 1) * 2
-        N = len(s)
+        increment = 2 * (numRows - 1)
         res = []
-        while top < N:
-            res.append(s[top])
-            top += (numRows - 1) * 2
-        for _ in range(numRows - 2):
-            start += 1
-            increment = ((numRows - 1) * 2) - (start * 2)
-            reverse_increment = start * 2
-            i = start
-            flip = True
-            while i < N:
-                add = increment if flip else reverse_increment
+        for r in range(numRows):
+            for i in range(r, len(s), increment):
                 res.append(s[i])
-                i += add
-                flip = not flip
-        while bottom < N:
-            res.append(s[bottom])
-            bottom += (numRows - 1) * 2
-        
+                if r > 0 and r < numRows - 1 and i + increment - (2 * r) < len(s):
+                    res.append(s[i + increment - (2 * r)])
+                    
         return "".join(res)
