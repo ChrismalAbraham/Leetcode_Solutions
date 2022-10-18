0127c37eb15e70157b21c2e0a0484292ae39c88c
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-18 18:30:24 -0400

    Time: 908 ms (38.04%), Space: 16.1 MB (38.62%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -1,62 +1,54 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        class SlidingWindowMedian:
+        class SlidingWindowMedian():
             def __init__(self):
-                self.maxHeap, self.minHeap = [], []
+                self.min_heap = []
+                self.max_heap = []
 
-            def find_sliding_window_median(self, nums, k):
-                result = [0.0 for x in range(len(nums) - k + 1)]
-                for i in range(0, len(nums)):
+            def find_sliding_window_median(self, nums: list, k: int) -> list:
+                median_list = [0.0 for x in range(len(nums) - k + 1)]
+                for i in range(len(nums)):
                     self.insert(nums[i])
 
-                    if i - k + 1 >= 0:  # if we have at least 'k' elements in the sliding window
-                        result[i - k + 1] = self.find_median()
+                    if i - k + 1 >= 0:  # if there are k elements in the window then calculate median
+                        median_list[i - k + 1] = self.find_median()
                         self.remove_element(nums[i - k + 1])
 
-                return result
+                return median_list
 
             def insert(self, num):
-                if not self.maxHeap or num <= -self.maxHeap[0]:
-                    heappush(self.maxHeap, -num)
+                if not self.max_heap or num < -self.max_heap[0]:
+                    heappush(self.max_heap, -num)
                 else:
-                    heappush(self.minHeap, num)
-
+                    heappush(self.min_heap, num)
                 self.rebalance_heaps()
 
-            # removes an element from the heap keeping the heap property
-            def remove(self, heap, element):
-                ind = heap.index(element)  # find the element
-                # move the element to the end and delete it
-                heap[ind] = heap[-1]
+            def remove_element(self, num):
+                if num <= -self.max_heap[0]:
+                    self.remove(self.max_heap, -num)
+                else:
+                    self.remove(self.min_heap, num)
+                self.rebalance_heaps()
+
+            def remove(self, heap, num):
+                idx = heap.index(num)
+                heap[idx] = heap[-1]
                 del heap[-1]
-                # we can use heapify to readjust the elements but that would be O(N),
-                # instead, we will adjust only one element which will O(logN)
-                if ind < len(heap):
-                    heapq._siftup(heap, ind)
-                    heapq._siftdown(heap, 0, ind)
-
-            def remove_element(self, element_to_remove):
-                if element_to_remove <= -self.maxHeap[0]:
-                    self.remove(self.maxHeap, -element_to_remove)
-                else:
-                    self.remove(self.minHeap, element_to_remove)
-
-                self.rebalance_heaps()
+                if idx < len(heap):
+                    heapq._siftup(heap, idx)
+                    heapq._siftdown(heap, 0, idx)
 
             def rebalance_heaps(self):
-                # either both the heaps will have equal number of elements or max-heap will have
-                # one more element than the min-heap
-                if len(self.maxHeap) > len(self.minHeap) + 1:
-                    heappush(self.minHeap, -heappop(self.maxHeap))
-                elif len(self.maxHeap) < len(self.minHeap):
-                    heappush(self.maxHeap, -heappop(self.minHeap))
+                if len(self.max_heap) > len(self.min_heap) + 1:
+                    heappush(self.min_heap, -heappop(self.max_heap))
+                elif len(self.max_heap) < len(self.min_heap):
+                    heappush(self.max_heap, -heappop(self.min_heap))
 
             def find_median(self) -> int:
-                if len(self.maxHeap) == len(self.minHeap):
-                    return -self.maxHeap[0] / 2.0 + self.minHeap[0] / 2.0
-                else:  # because max-heap will have one more element than the min-heap
-                    return -self.maxHeap[0] / 1.
+                if len(self.max_heap) == len(self.min_heap):
+                    return -self.max_heap[0] / 2.0 + self.min_heap[0] / 2.0
+                else:
+                    return -self.max_heap[0] / 1.0
 
-
-        result = SlidingWindowMedian()
-        return result.find_sliding_window_median(nums, k)
+        sliding_window_list = SlidingWindowMedian()
+        return sliding_window_list.find_sliding_window_median(nums, k)
