a32d16f5f78b42ca86553b172fd6640b1e4bc773
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-20 20:31:56 -0400

    Time: 73 ms (56.34%), Space: 13.9 MB (84.53%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,34 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        len_nums = len(nums)
-        permutations = deque()
-        permutations.append([])
-        for num in nums:
-            len_permutations = len(permutations)
-            for i in range(len_permutations):
-                curr_permutation = permutations.popleft()
+        def find_permutations(res, index, curr_permutation, nums):
+            if index == len(nums):
+                res.append(curr_permutation)
+            else:
                 for j in range(len(curr_permutation) + 1):
                     new_permutation = list(curr_permutation)
-                    new_permutation.insert(j, num)
-                    if len(new_permutation) == len_nums:
-                        res.append(new_permutation)
-                    else:
-                        permutations.append(new_permutation)
-
+                    new_permutation.insert(j, nums[index])
+                    find_permutations(res, index + 1, new_permutation, nums)
+        res = []
+        find_permutations(res, 0, [], nums)
         return res
+        
+        
+#         res = []
+#         len_nums = len(nums)
+#         permutations = deque()
+#         permutations.append([])
+#         for num in nums:
+#             len_permutations = len(permutations)
+#             for i in range(len_permutations):
+#                 curr_permutation = permutations.popleft()
+#                 for j in range(len(curr_permutation) + 1):
+#                     new_permutation = list(curr_permutation)
+#                     new_permutation.insert(j, num)
+#                     if len(new_permutation) == len_nums:
+#                         res.append(new_permutation)
+#                     else:
+#                         permutations.append(new_permutation)
+
+#         return res
         
         
