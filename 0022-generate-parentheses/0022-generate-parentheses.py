39ba83fb5d375b7956986372ad5357c0166524ca
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-24 19:02:37 -0400

    Time: 87 ms (10.59%), Space: 14.4 MB (5.77%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generateParenthesis(self, num: int) -> List[str]:
+        class ParanthesisString():
+            def __init__(self, string="", num_open=0, num_closed=0):
+                self.string = string
+                self.open_paran = num_open
+                self.closed_paran = num_closed
+                
+        res = []
+        queue = deque()
+        queue.append(ParanthesisString())
+        while queue:
+            curr = queue.popleft()
+            if curr.open_paran == num and curr.closed_paran == num:
+                res.append(curr.string)
+
+            else:
+                if curr.open_paran < num:
+                    new_string = curr.string + '('
+                    queue.append(ParanthesisString(
+                        new_string, curr.open_paran + 1, curr.closed_paran))
+
+                if curr.closed_paran < curr.open_paran:
+                    new_string = curr.string + ')'
+                    queue.append(ParanthesisString(
+                        new_string, curr.open_paran, curr.closed_paran + 1))
+
+        return res
