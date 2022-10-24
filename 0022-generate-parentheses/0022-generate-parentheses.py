5901713adadcc832bee1ca7762875484b6927cb1
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-24 19:25:59 -0400

    Time: 60 ms (63.08%), Space: 14.3 MB (40.00%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,22 +5,39 @@ class Solution:
                 self.string = string
                 self.open_paran = num_open
                 self.closed_paran = num_closed
-                
-        res = []
-        queue = deque()
-        queue.append(ParanthesisString())
-        while queue:
-            curr = queue.popleft()
-            if curr.open_paran == num and curr.closed_paran == num:
-                res.append(curr.string)
-
+       
+        def all_valid_paranthesis(num, res, curr_ps):
+            if curr_ps.open_paran == num and curr_ps.closed_paran == num:
+                res.append(curr_ps.string)
             else:
-                if curr.open_paran < num:
-                    queue.append(ParanthesisString(
-                        curr.string + '(', curr.open_paran + 1, curr.closed_paran))
-
-                if curr.closed_paran < curr.open_paran:
-                    queue.append(ParanthesisString(
-                        curr.string + ')', curr.open_paran, curr.closed_paran + 1))
-
+                if curr_ps.open_paran < num:
+                    all_valid_paranthesis(num, res, ParanthesisString(
+                        curr_ps.string + '(', curr_ps.open_paran + 1, curr_ps.closed_paran))
+                if curr_ps.closed_paran < curr_ps.open_paran:
+                    all_valid_paranthesis(num, res, ParanthesisString(
+                        curr_ps.string + ')', curr_ps.open_paran, curr_ps.closed_paran + 1))
+        res = []
+        all_valid_paranthesis(num, res, ParanthesisString())
         return res
+    
+    
+    
+#         res = []
+#         queue = deque()
+#         queue.append(ParanthesisString())
+#         while queue:
+#             curr = queue.popleft()
+#             if curr.open_paran == num and curr.closed_paran == num:
+#                 res.append(curr.string)
+
+#             else:
+#                 if curr.open_paran < num:
+#                     queue.append(ParanthesisString(
+#                         curr.string + '(', curr.open_paran + 1, curr.closed_paran))
+
+#                 if curr.closed_paran < curr.open_paran:
+#                     new_string = curr.string + ')'
+#                     queue.append(ParanthesisString(
+#                         curr.string + ')' , curr.open_paran, curr.closed_paran + 1))
+
+#         return res
