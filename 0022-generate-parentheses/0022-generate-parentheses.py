60891e441de688b0c996d56cb56f3fc2d2332a5c
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-24 19:41:02 -0400

    Time: 72 ms (32.64%), Space: 14.3 MB (40.00%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,27 +1,29 @@
 class Solution:
     def generateParenthesis(self, num: int) -> List[str]:
-        class ParanthesisString():
-            def __init__(self, string="", num_open=0, num_closed=0):
-                self.string = string
-                self.open_paran = num_open
-                self.closed_paran = num_closed
-       
-        def all_valid_paranthesis(num, res, curr_ps):
-            if curr_ps.open_paran == num and curr_ps.closed_paran == num:
-                res.append(curr_ps.string)
+        
+        def all_valid_paranthesis(num, res, p_string, open_paran, closed_paran, index):
+            if open_paran == num and closed_paran == num:
+                res.append(''.join(p_string))
             else:
-                if curr_ps.open_paran < num:
-                    all_valid_paranthesis(num, res, ParanthesisString(
-                        curr_ps.string + '(', curr_ps.open_paran + 1, curr_ps.closed_paran))
-                if curr_ps.closed_paran < curr_ps.open_paran:
-                    all_valid_paranthesis(num, res, ParanthesisString(
-                        curr_ps.string + ')', curr_ps.open_paran, curr_ps.closed_paran + 1))
+                if open_paran < num:
+                    p_string[index] = '('
+                    all_valid_paranthesis(num, res, p_string,
+                                          open_paran + 1, closed_paran, index + 1)
+                if closed_paran < open_paran:
+                    p_string[index] = ')'
+                    all_valid_paranthesis(num, res, p_string,
+                                          open_paran, closed_paran + 1, index + 1)
         res = []
-        all_valid_paranthesis(num, res, ParanthesisString())
+        p_string = [0 for x in range(num * 2)]
+        all_valid_paranthesis(num, res, p_string, 0, 0, 0)
         return res
     
     
-    
+#         class ParanthesisString():
+#             def __init__(self, string="", num_open=0, num_closed=0):
+#             self.string = string
+#             self.open_paran = num_open
+#             self.closed_paran = num_closed   
 #         res = []
 #         queue = deque()
 #         queue.append(ParanthesisString())
