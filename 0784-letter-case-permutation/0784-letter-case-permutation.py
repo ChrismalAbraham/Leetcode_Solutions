b869fe7c66b81e247f5aa6fd2655556faa6428d6
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-24 17:51:41 -0400

    Time: 136 ms (28.66%), Space: 14.7 MB (59.82%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCasePermutation(self, arr: str) -> List[str]:
+        def is_letter(char: str) -> bool:
+            if char.isalpha():
+                return True
+            return False
+
+        def switch_case(char: str) -> str:
+            if char.islower():
+                return char.upper()
+            return char.lower()
+
+        res = []
+        res.append(arr)
+        pos_of_char = []
+        for i, char in enumerate(arr):
+            if is_letter(char):
+                pos_of_char.append(i)
+
+        for index_of_char in pos_of_char:
+            len_permutations = len(res)
+            for j in range(len_permutations):
+                new_permutation = list(res[j])
+                new_permutation[index_of_char] = switch_case(
+                    new_permutation[index_of_char])
+                res.append("".join(new_permutation))
+
+        return res
