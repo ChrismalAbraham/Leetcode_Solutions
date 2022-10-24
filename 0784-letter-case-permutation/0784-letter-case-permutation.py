fc3ae4154d69350d90afc1808b8a951463dd1577
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-10-24 18:07:11 -0400

    Time: 120 ms (47.11%), Space: 14.6 MB (97.64%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,27 +1,12 @@
 class Solution:
     def letterCasePermutation(self, arr: str) -> List[str]:
-        def is_letter(char: str) -> bool:
-            if char.isalpha():
-                return True
-            return False
+        permutations = [arr]
+        for i in range(len(arr)):
+            if arr[i].isalpha():
+                len_permutations = len(permutations)
+                for j in range(len_permutations):
+                    new_permutation = list(permutations[j])
+                    new_permutation[i] = new_permutation[i].swapcase()
+                    permutations.append("".join(new_permutation))
 
-        def switch_case(char: str) -> str:
-            if char.islower():
-                return char.upper()
-            return char.lower()
-
-        res = [arr]
-        pos_of_char = []
-        for i, char in enumerate(arr):
-            if is_letter(char):
-                pos_of_char.append(i)
-
-        for index_of_char in pos_of_char:
-            len_permutations = len(res)
-            for j in range(len_permutations):
-                new_permutation = list(res[j])
-                new_permutation[index_of_char] = switch_case(
-                    new_permutation[index_of_char])
-                res.append("".join(new_permutation))
-
-        return res
+        return permutations
