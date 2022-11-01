d1adfeb8152fddeb98bac92833348020c8184a1b
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-11-01 19:19:03 -0400

    Time: 254 ms (31.63%), Space: 18.6 MB (91.60%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,37 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-        
+# Using a min_heap and a hashmap
+        freq_map = defaultdict(int)
         for num in nums:
-            count[num] = count.get(num, 0) + 1
-        for n, c in count.items():
-            freq[c].append(n)
+            freq_map[num] += 1
+
+        min_heap = []
+        for val, freq in freq_map.items():
+            heappush(min_heap, (freq, val))
+            if len(min_heap) > k:
+                heappop(min_heap)
+
+        res = []
+        for i in range(k):
+            res.append(heappop(min_heap)[1])
+        return res
         
-        response = []
-        for i in range(len(freq) - 1, 0, -1):
-            for num in freq[i]:
-                response.append(num)
-                if len(response) == k:
-                    return response
+# Using an array and a Hashmap        
+#         count = {}
+#         freq = [[] for i in range(len(nums) + 1)]
+        
+#         for num in nums:
+#             count[num] = count.get(num, 0) + 1
+#         for n, c in count.items():
+#             freq[c].append(n)
+        
+#         response = []
+#         for i in range(len(freq) - 1, 0, -1):
+#             for num in freq[i]:
+#                 response.append(num)
+#                 if len(response) == k:
+#                     return response
         
         
         
