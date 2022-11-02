68d9021f8bfeec6e93d7aad8ad3e2c0b2a232dba
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-11-02 18:38:34 -0400

    Time: 1058 ms (7.79%), Space: 16 MB (21.12%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,18 +1,18 @@
 class Solution:
     def findClosestElements(self, nums: List[int], k: int, x: int) -> List[int]:
-        def distance(elem: int, x) -> int:
-            # nonlocal x
+        def distance(elem: int) -> int:
+            nonlocal x
             return abs(elem - x)
 
         max_heap = []
         for i in range(k):
-            heappush(max_heap, (-distance(nums[i], x), nums[i]))
+            heappush(max_heap, (-distance(nums[i]), nums[i]))
 
         for i in range(k, len(nums)):
-            num_dist = distance(nums[i], x)
+            num_dist = distance(nums[i])
             if num_dist < -max_heap[0][0]:
                 heappop(max_heap)
-                heappush(max_heap, (-distance(nums[i], x), nums[i]))
+                heappush(max_heap, (-distance(nums[i]), nums[i]))
 
         res = []
         for dist, num in max_heap:
