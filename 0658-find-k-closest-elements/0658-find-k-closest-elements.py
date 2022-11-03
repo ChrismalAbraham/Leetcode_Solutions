bb8466abd9494939d8662507b3059eacc35119b5
ChrismalAbraham <50206458+ChrismalAbraham@users.noreply.github.com>
2022-11-02 20:28:26 -0400

    Time: 950 ms (14.50%), Space: 15.6 MB (27.45%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findClosestElements(self, nums: List[int], num_of_elem: int, target: int) -> List[int]:
+    def findClosestElements(self, nums: List[int], nums_of_elem: int, target: int) -> List[int]:
         def binary_search(nums, target):
             start, end = 0, len(nums) - 1
             while start <= end:
@@ -10,19 +10,58 @@ class Solution:
                     start = mid + 1
                 else:
                     return mid
-            return mid
+            if start > 0:
+                return start -1
+            return start
 
-        closest_index_to_k = binary_search(nums, target)
-        high, low = closest_index_to_k + num_of_elem, closest_index_to_k - num_of_elem
-        # don't want the ends of our ranges to be outside the array bounds when indexing
-        high, low = min(high, len(nums) - 1), max(low, 0)
+        def dist(num):
+            nonlocal target
+            return abs(num - target)
 
-        min_heap = []
-        for i in range(low, high + 1):
-            heappush(min_heap, (abs(nums[i] - target), nums[i]))
+        closest_idx = binary_search(nums, target)
 
-        res = []
-        for i in range(num_of_elem):
-            res.append(heappop(min_heap)[1])
-        res.sort()
-        return res
+        left, right = closest_idx, closest_idx + 1
+        queue = deque()
+
+        while len(queue) < nums_of_elem:
+            if left < 0:
+                queue.append(nums[right])
+                right += 1
+            elif right >= len(nums):
+                queue.appendleft(nums[left])
+                left -= 1
+            elif dist(nums[left]) <= dist(nums[right]):
+                queue.appendleft(nums[left])
+                left -= 1
+            else:
+                queue.append(nums[right])
+                right += 1
+
+        return queue
+
+#         def binary_search(nums, target):
+#             start, end = 0, len(nums) - 1
+#             while start <= end:
+#                 mid = start + (end - start) // 2
+#                 if target < nums[mid]:
+#                     end = mid - 1
+#                 elif target > nums[mid]:
+#                     start = mid + 1
+#                 else:
+#                     return mid
+#             return mid
+
+#         closest_index_to_k = binary_search(nums, target)
+#         high, low = closest_index_to_k + num_of_elem, closest_index_to_k - num_of_elem
+#         # don't want the ends of our ranges to be outside the array bounds when indexing
+#         high, low = min(high, len(nums) - 1), max(low, 0)
+
+#         min_heap = []
+#         for i in range(low, high + 1):
+#             heappush(min_heap, (abs(nums[i] - target), nums[i]))
+
+#         res = []
+#         for i in range(num_of_elem):
+#             res.append(heappop(min_heap)[1])
+#         res.sort()
+#         return res
